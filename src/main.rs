a9d832df2a2003484fdeb324efecd533d7d2ce06
Amaury Martiny <amaury.martiny@protonmail.com>
2018-08-30 00:47:33 +0200

    Add cli to the program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // Calling .unwrap() is safe here because "EMAIL" is required
+    let email = matches.value_of("EMAIL").unwrap();
+
+    println!("{}", email)
 }
