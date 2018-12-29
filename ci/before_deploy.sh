e668be20e4dd60747f7595c010c2842f024c96ef
Amaury Martiny <amaury.martiny@protonmail.com>
2018-12-29 16:31:38 +0100

    Change binary name

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,10 +18,10 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
+    cross rustc --bin check_if_email_exists --target $TARGET --release -- -C lto
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
+    cp target/$TARGET/release/check_if_email_exists $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
