3b4e4bc6748b82c5338983599b5cc04a0cc14844
Amaury Martiny <amaury.martiny@protonmail.com>
2019-03-24 23:04:32 +0100

    Update script to work on both platforms

--- ci/script.sh
+++ ci/script.sh
@@ -16,8 +16,10 @@ main() {
 
     # Run the binary with a $TEST_EMAIL, to test that the tool actually works.
     # TODO This only works on osx now, see #11
-    [ "$TRAVIS_OS_NAME" = osx ] && cross run --target $TARGET $TEST_EMAIL | grep true
-    [ "$TRAVIS_OS_NAME" = osx ] && cross run --target $TARGET --release $TEST_EMAIL | grep true
+    if [ $TRAVIS_OS_NAME = osx ]; then
+        cross run --target $TARGET $TEST_EMAIL | grep true
+        cross run --target $TARGET --release $TEST_EMAIL | grep true
+    fi
 }
 
 # we don't run the "test phase" when doing deploys
