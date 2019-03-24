8b780a0e98faa7f1df5418e146133310f8b9117c
Amaury Martiny <amaury.martiny@protonmail.com>
2019-03-24 21:50:31 +0100

    Run test email on travis

--- ci/script.sh
+++ ci/script.sh
@@ -16,8 +16,8 @@ main() {
 
     # TODO run with a $TEST_EMAIL, to test that the tool actually works. Right
     # now, Travis blocks ports 25, 587 and 465, so we cannot do that.
-    # cross run --target $TARGET
-    # cross run --target $TARGET --release
+    cross run --target $TARGET $TEST_EMAIL
+    cross run --target $TARGET --release $TEST_EMAIL
 }
 
 # we don't run the "test phase" when doing deploys
