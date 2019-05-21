6b32272a7a6ab6e0d06fd0c6490b0814bbeed57e
Amaury Martiny <amaury.martiny@protonmail.com>
2019-05-21 09:34:03 +0200

    chore: Change license to gpl v3

--- src/smtp.rs
+++ src/smtp.rs
@@ -1,3 +1,19 @@
+// check_if_email_exists
+// Copyright (C) 2018-2019 Amaury Martiny
+
+// check_if_email_exists is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// check_if_email_exists is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with check_if_email_exists.  If not, see <http://www.gnu.org/licenses/>.
+
 use lettre::smtp::client::net::NetworkStream;
 use lettre::smtp::client::InnerClient;
 use lettre::smtp::commands::*;
