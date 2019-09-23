4c808a3767cbaaa57d664b63595ee18965f873c6
Amaury Martiny <amaury.martiny@protonmail.com>
2019-09-23 17:11:23 +0200

    Read body

--- src/cli/http.rs
+++ src/cli/http.rs
@@ -1,3 +1,19 @@
+// check_if_email_exists
+// Copyright (C) 2018-2019 Amaury Martiny
+
+// check_if_email_exists is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// check_if_email_exists is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with check_if_email_exists.  If not, see <http://www.gnu.org/licenses/>.
+
 use check_if_email_exists::email_exists;
 use futures::future;
 use futures::Stream;
@@ -5,42 +21,56 @@ use hyper::rt::Future;
 use hyper::service::service_fn;
 use hyper::{Body, Request, Response, Server};
 use hyper::{Method, StatusCode};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
 /// JSON Request from POST /
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct PostReqBody {
 	from_email: String,
 	to_email: String,
+}
+
+/// Error Response from POST /
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PostResError {
+	error: String,
 }
 fn req_handler(req: Request<Body>) -> BoxFut {
 	let mut response = Response::new(Body::empty());
 
 	match (req.method(), req.uri().path()) {
+		// Show instructions on GET /
 		(&Method::GET, "/") => {
 			*response.body_mut() =
-				Body::from("Send a POST request with JSON `{ from_email: <email>, to_email: <email> }` in the body");
+				Body::from("Send a POST request with JSON `{ \"from_email\": \"<email>\", to_email: \"<email>\" }` in the body");
 		}
+
+		// Do email_exists check on POST /
 		(&Method::POST, "/") => {
-			println!("HELLO");
-			let body = req.into_body().concat2().wait().unwrap().into_bytes();
-			println!("{:?}", body);
-			match serde_json::from_slice::<PostReqBody>(&body) {
-				Ok(b) => {
-					println!("{:?}", b);
-					*response.body_mut() = Body::from(
-						serde_json::to_string(&email_exists(&b.from_email, &b.to_email)).unwrap(),
-					);
-				}
-				Err(_err) => {
-					println!("{:?}", _err);
-					*response.status_mut() = StatusCode::UNPROCESSABLE_ENTITY;
-					*response.body_mut() = Body::from("Error in input");
-				}
-			}
+			let checked = req.into_body().concat2().map(move |chunk| {
+				let body = &chunk.iter().cloned().collect::<Vec<u8>>();
+				let result = match serde_json::from_slice::<PostReqBody>(body) {
+					Ok(b) => {
+						println!("YEAH! {:?}", b);
+						serde_json::to_string(&email_exists(&b.from_email, &b.to_email)).unwrap()
+					}
+
+					Err(err) => serde_json::to_string(&PostResError {
+						error: format!("{}", err),
+					})
+					.unwrap(),
+				};
+
+				*response.body_mut() = Body::from(result);
+				response
+			});
+
+			return Box::new(checked);
 		}
+
+		// 404 page on all other routes
 		_ => {
 			*response.status_mut() = StatusCode::NOT_FOUND;
 		}
