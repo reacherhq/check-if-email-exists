3d4e45bd020419cac0fab1ae02e8ae10a7717faf
Amaury Martiny <amaury.martiny@protonmail.com>
2019-10-21 12:02:13 +0200

    Restyle to kebab-case

--- serverless/src/main.rs
+++ serverless/src/main.rs
@@ -1,18 +1,18 @@
-// check_if_email_exists
+// check-if-email-exists
 // Copyright (C) 2018-2019 Amaury Martiny
 
-// check_if_email_exists is free software: you can redistribute it and/or modify
+// check-if-email-exists is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// check_if_email_exists is distributed in the hope that it will be useful,
+// check-if-email-exists is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with check_if_email_exists.  If not, see <http://www.gnu.org/licenses/>.
+// along with check-if-email-exists.  If not, see <http://www.gnu.org/licenses/>.
 
 extern crate lambda_http;
 extern crate lambda_runtime;
