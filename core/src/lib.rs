dcf9d60da405e67ad4ce425731e1ec3206d9095e
Amaury Martiny <amaury.martiny@protonmail.com>
2020-01-06 13:52:01 +0100

    chore: Update license to 2020 (#143)

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,5 @@
 // check-if-email-exists
-// Copyright (C) 2018-2019 Amaury Martiny
+// Copyright (C) 2018-2020 Amaury Martiny
 
 // check-if-email-exists is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
