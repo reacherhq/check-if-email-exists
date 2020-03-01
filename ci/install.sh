92edcfcd73538569aca57815ebf4477817e7ac68
Amaury Martiny <amaury.martiny@protonmail.com>
2020-03-01 10:16:07 +0100

    Use specific tag

--- ci/install.sh
+++ ci/install.sh
@@ -36,11 +36,13 @@ main() {
                        | grep -E '^v[0.1.0-9.]+$' \
                        | $sort --version-sort \
                        | tail -n1)
+    # We use v0.1.9 specifically, starting from v0.2.0, Travis errors
+    # https://github.com/amaurymartiny/check-if-email-exists/issues/211
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
            --git japaric/cross \
-           --tag $tag \
+           --tag v0.1.9 \
            --target $target
 }
 
