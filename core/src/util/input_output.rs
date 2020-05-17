310db07c00edc5d43bd087c0075b77353ae34860
Amaury Martiny <amaury.martiny@protonmail.com>
2020-05-18 00:50:36 +0200

    chore: Update LICENSE (#353)
    * Update LICENSE
    
    * AGPL

--- core/src/util/input_output.rs
+++ core/src/util/input_output.rs
@@ -1,18 +1,18 @@
 // check-if-email-exists
 // Copyright (C) 2018-2020 Amaury Martiny
 
-// check-if-email-exists is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// check-if-email-exists is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Affero General Public License for more details.
 
-// You should have received a copy of the GNU General Public License
-// along with check-if-email-exists.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use crate::misc::{MiscDetails, MiscError};
 use crate::mx::{MxDetails, MxError};
