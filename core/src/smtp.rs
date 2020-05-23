856777a5fbfd687bb9b9d685dac228f46a82f124
Amaury Martiny <amaury.martiny@protonmail.com>
2020-05-23 11:55:55 +0200

    fix: Add additional error check for undeliverable

--- core/src/smtp.rs
+++ core/src/smtp.rs
@@ -246,6 +246,8 @@ async fn email_deliverable(
 				// 550 5.1.1 No such user - pp
 				// 550 No such user here
 				|| err_string.contains("no such user")
+				// 4.2.1 The user you are trying to contact is receiving mail at a rate...
+				|| err_string.contains("is receiving mail at a rate")
 				// 550 5.1.1 : Mailbox not found
 				// 550 Unknown address error ‘MAILBOX NOT FOUND’
 				|| err_string.contains("not found")
