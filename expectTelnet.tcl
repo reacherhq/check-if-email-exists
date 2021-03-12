7a3f70df429a5830c446f6a4f69df51ab3846443
Anagastes <Jacky_009@gmx.de>
2021-03-12 14:01:32 +0100

    Update expectTelnet.tcl
    extended to fit with the commit from https://github.com/Anagastes/check-if-email-exists/commit/30dc4cdb6726c25ba4e00ffe8942455f65371ac0

--- expectTelnet.tcl
+++ expectTelnet.tcl
@@ -5,15 +5,16 @@ set email [lindex $argv 0]
 set host [lindex $argv 1]
 set port [lindex $argv 2]
 set sender [lindex $argv 3]
+set sender_host [lindex $argv 4]
 
 # Start a Telnet communication
 spawn telnet $host $port;
 expect "220"
-send "HELO Hi\r";
+send "EHLO $sender_host\r";
 expect "250"
-send "MAIL FROM: <$sender>\r";
+send "MAIL FROM: $sender\r";
 expect "250"
-send "RCPT TO: <$email>\r";
+send "RCPT TO: $email\r";
 expect "*"
 send "QUIT\r"
 expect eof
