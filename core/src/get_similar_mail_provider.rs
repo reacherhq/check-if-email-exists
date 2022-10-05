c02dd49a8f1c941bb9b298c95c73782f1949ff7b
Daniel Huth <4455258+Agreon@users.noreply.github.com>
2022-10-06 10:26:19 +1300

    Add suggested PR feedback

--- core/src/get_similar_mail_provider.rs
+++ core/src/get_similar_mail_provider.rs
@@ -2,29 +2,20 @@ use levenshtein::levenshtein;
 
 use crate::syntax::SyntaxDetails;
 
-const MAIL_PROVIDERS: &[&str] = &["gmail", "yahoo"];
+const MAIL_PROVIDERS: &[&str] = &["gmail.yom", "yahoo.yom", "outlook.com", "hotmail.com"];
 
+// Supplies the syntax parameter with a suggestion that matches the mail domain best by levenshtein
+// distance.
 pub fn get_similar_mail_provider(syntax: &mut SyntaxDetails) {
-	let domain: &str = syntax.domain.as_ref();
-	let mut domain_parts = domain.split('.');
-
-	let provider = domain_parts
-		.next()
-		.expect("We already checked that the syntax is valid");
-	let tld = domain_parts
-		.next()
-		.expect("We already checked that the syntax is valid");
-
 	for possible_provider in MAIL_PROVIDERS {
-		let distance = levenshtein(provider, possible_provider);
+		let distance = levenshtein(&syntax.domain, possible_provider);
 
 		if distance < 3 {
 			// Return full address
 			syntax.suggestion = Some(format!(
-				"{}@{}.{}",
+				"{}@{}",
 				syntax.username,
 				String::from(*possible_provider),
-				tld
 			));
 			break;
 		}
