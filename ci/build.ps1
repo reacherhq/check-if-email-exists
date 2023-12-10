ab2bb4184adbd77628a38de6dccf01a1fde029cb
Amaury <1293565+amaury1729@users.noreply.github.com>
2023-12-10 18:57:57 +0100

    fix(ci): Fix Windows build (#1397)
    * fix(ci): Fix Windows build
    
    * fix?
    
    * fix?

--- ci/build.ps1
+++ ci/build.ps1
@@ -2,9 +2,12 @@ $cross = $args[0]
 $target_triple = $args[1]
 $release_build = $args[2]
 
+# reacher_worker doesn't compile on windows because of this bug:
+# https://github.com/amqp-rs/reactor-trait/issues/1
+# So we only build the check_if_email_exists binary for Windows.
 if ( $release_build -ne "RELEASE" ) {
-    Invoke-Expression "$cross build --target $target_triple"
-    Invoke-Expression "$cross build --target $target_triple --all-features"
+    Invoke-Expression "$cross build --bin check_if_email_exists --target $target_triple"
+    Invoke-Expression "$cross build --bin check_if_email_exists --target $target_triple --all-features"
 } else {
-    Invoke-Expression "$cross build --target $target_triple --all-features --release"
+    Invoke-Expression "$cross build --bin check_if_email_exists --target $target_triple --all-features --release"
 }
