84411b384f6503891ddaaf3b62d9e58e946f54db
Amaury <1293565+amaury1729@users.noreply.github.com>
2023-12-27 15:08:08 +0100

    Make lapin optional

--- backend/src/http/mod.rs
+++ backend/src/http/mod.rs
@@ -22,6 +22,7 @@ use std::env;
 use std::net::IpAddr;
 
 use check_if_email_exists::LOG_TARGET;
+#[cfg(feature = "worker")]
 use lapin::Channel;
 use tracing::info;
 use warp::Filter;
@@ -29,8 +30,8 @@ use warp::Filter;
 use super::errors;
 
 #[cfg(feature = "worker")]
-pub fn create_routes(
-	channel: Option<Channel>,
+fn create_routes_with_bulk(
+	channel: Channel,
 ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
 	version::get::get_version()
 		.or(v0::check_email::post::post_check_email())
@@ -38,9 +39,9 @@ pub fn create_routes(
 		.recover(errors::handle_rejection)
 }
 
-#[cfg(not(feature = "worker"))]
-pub fn create_routes(
-	_channel: Option<Channel>,
+/// Creates the routes for the HTTP server.
+/// Making it public so that it can be used in tests/check_email.rs.
+pub fn create_routes_without_bulk(
 ) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
 	version::get::get_version()
 		.or(v0::check_email::post::post_check_email())
@@ -52,7 +53,7 @@ pub fn create_routes(
 /// This function starts the Warp server and listens for incoming requests.
 /// It returns a `Result` indicating whether the server started successfully or encountered an error.
 pub async fn run_warp_server(
-	channel: Option<Channel>,
+	#[cfg(feature = "worker")] channel: Channel,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 	let host = env::var("RCH_HTTP_HOST")
 		.unwrap_or_else(|_| "127.0.0.1".into())
@@ -65,7 +66,10 @@ pub async fn run_warp_server(
 		})
 		.unwrap_or(8080);
 
-	let routes = create_routes(channel);
+	#[cfg(feature = "worker")]
+	let routes = create_routes_with_bulk(channel);
+	#[cfg(not(feature = "worker"))]
+	let routes = create_routes_without_bulk();
 
 	info!(target: LOG_TARGET, host=?host,port=?port, "Server is listening");
 	warp::serve(routes).run((host, port)).await;
