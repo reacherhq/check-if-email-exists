75ba303efecac12a3462edfee259c6e5f5b8867a
Amaury <1293565+amaury1729@users.noreply.github.com>
2024-01-16 13:00:42 +0100

    chore(backend): Update crate version to 0.7.0

--- backend/migrations/20231227103745_init.sql
+++
@@ -1,8 +0,0 @@
--- Add migration script here
-CREATE TABLE email_results (
-    id SERIAL NOT NULL PRIMARY KEY,
-    email TEXT NOT NULL,
-    is_reachable VARCHAR(10) NOT NULL,
-    full_result jsonb NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT NOW()
-);
