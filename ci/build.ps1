9c5324f0599a89ef0c82d0aa3aea7193d5e3c5eb
Amaury <1293565+amaury1093@users.noreply.github.com>
2024-10-06 00:14:39 +0200

    try

--- ci/build.ps1
+++ ci/build.ps1
@@ -2,9 +2,10 @@ $cross = $args[0]
 $target_triple = $args[1]
 $release_build = $args[2]
 
+# Only the CLI and backend are supported for now on Windows
 if ( $release_build -ne "RELEASE" ) {
-    Invoke-Expression "$cross build --target $target_triple"
-    Invoke-Expression "$cross build --target $target_triple --all-features"
+    Invoke-Expression "$cross build -p cli -p backend --target $target_triple"
+    Invoke-Expression "$cross build -p cli -p backend --target $target_triple --all-features"
 } else {
-    Invoke-Expression "$cross build --target $target_triple --all-features --release"
+    Invoke-Expression "$cross build -p cli -p backend --target $target_triple --all-features --release"
 }
