52da082506626ce77742aeaf03b96369e209c4d3
Amaury <1293565+amaury1093@users.noreply.github.com>
2024-10-06 00:24:15 +0200

    typo

--- ci/build.ps1
+++ ci/build.ps1
@@ -4,8 +4,8 @@ $release_build = $args[2]
 
 # Only the CLI and is supported for now on Windows
 if ( $release_build -ne "RELEASE" ) {
-    Invoke-Expression "$cross build --bin check-if-email-exists --target $target_triple"
-    Invoke-Expression "$cross build --bin check-if-email-exists --target $target_triple --all-features"
+    Invoke-Expression "$cross build --bin check_if_email_exists --target $target_triple"
+    Invoke-Expression "$cross build --bin check_if_email_exists --target $target_triple --all-features"
 } else {
-    Invoke-Expression "$cross build --bin check-if-email-exists --target $target_triple --all-features --release"
+    Invoke-Expression "$cross build --bin check_if_email_exists --target $target_triple --all-features --release"
 }
