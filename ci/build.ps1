eba8a7a97ae83fadba48517291dd2e57d0d5feaa
Amaury <1293565+amaury1093@users.noreply.github.com>
2024-10-06 12:12:42 +0200

    skip windows

--- ci/build.ps1
+++ ci/build.ps1
@@ -2,10 +2,9 @@ $cross = $args[0]
 $target_triple = $args[1]
 $release_build = $args[2]
 
-# Only the CLI and is supported for now on Windows
 if ( $release_build -ne "RELEASE" ) {
-    Invoke-Expression "$cross build --bin check_if_email_exists --target $target_triple"
-    Invoke-Expression "$cross build --bin check_if_email_exists --target $target_triple --all-features"
+    Invoke-Expression "$cross build --target $target_triple"
+    Invoke-Expression "$cross build --target $target_triple --all-features"
 } else {
-    Invoke-Expression "$cross build --bin check_if_email_exists --target $target_triple --all-features --release"
+    Invoke-Expression "$cross build --target $target_triple --all-features --release"
 }
