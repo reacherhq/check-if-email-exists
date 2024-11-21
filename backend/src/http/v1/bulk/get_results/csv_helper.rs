5bb1d02bf3d7c53f218514890fe53b7f07daa007
Amaury <1293565+amaury1093@users.noreply.github.com>
2024-11-21 18:18:35 +0100

    refactor csv_helper

--- backend/src/http/v1/bulk/get_results/csv_helper.rs
+++ backend/src/http/v1/bulk/get_results/csv_helper.rs
@@ -55,134 +55,97 @@ pub struct CsvResponse {
 	error: Option<String>,
 }
 
-/// Convert csv wrapper to csv response
-/// Performs multiple allocations for string fields
-/// throw error if field is missing
 impl TryFrom<CsvWrapper> for CsvResponse {
 	type Error = &'static str;
 
 	fn try_from(value: CsvWrapper) -> Result<Self, Self::Error> {
-		let mut input: String = String::default();
-		let mut is_reachable: String = String::default();
-		let mut misc_is_disposable: bool = false;
-		let mut misc_is_role_account: bool = false;
-		let mut misc_gravatar_url: Option<String> = None;
-		let mut mx_accepts_mail: bool = false;
-		let mut smtp_can_connect: bool = false;
-		let mut smtp_has_full_inbox: bool = false;
-		let mut smtp_is_catch_all: bool = false;
-		let mut smtp_is_deliverable: bool = false;
-		let mut smtp_is_disabled: bool = false;
-		let mut syntax_is_valid_syntax: bool = false;
-		let mut syntax_domain: String = String::default();
-		let mut syntax_username: String = String::default();
-		let mut error: Option<String> = None;
-
 		let top_level = value
 			.0
 			.as_object()
 			.ok_or("Failed to find top level object")?;
-		for (key, val) in top_level.keys().zip(top_level.values()) {
-			match key.as_str() {
-				"input" => input = val.as_str().ok_or("input should be a string")?.to_string(),
-				"is_reachable" => {
-					is_reachable = val
-						.as_str()
-						.ok_or("is_reachable should be a string")?
-						.to_string()
-				}
-				"misc" => {
-					let misc_obj = val.as_object().ok_or("misc field should be an object")?;
-					for (key, val) in misc_obj.keys().zip(misc_obj.values()) {
-						match key.as_str() {
-							"error" => error = Some(val.to_string()),
-							"is_disposable" => {
-								misc_is_disposable =
-									val.as_bool().ok_or("is_disposable should be a boolean")?
-							}
-							"is_role_account" => {
-								misc_is_role_account =
-									val.as_bool().ok_or("is_role_account should be a boolean")?
-							}
-							"gravatar_url" => {
-								if Option::is_some(&val.as_str()) {
-									misc_gravatar_url = Some(val.to_string())
-								}
-							}
-							_ => {}
-						}
-					}
-				}
-				"mx" => {
-					let mx_obj = val.as_object().ok_or("mx field should be an object")?;
-					for (key, val) in mx_obj.keys().zip(mx_obj.values()) {
-						match key.as_str() {
-							"error" => error = Some(val.to_string()),
-							"accepts_email" => {
-								mx_accepts_mail =
-									val.as_bool().ok_or("accepts_email should be a boolean")?
-							}
-							_ => {}
-						}
-					}
-				}
-				"smtp" => {
-					let smtp_obj = val.as_object().ok_or("mx field should be an object")?;
-					for (key, val) in smtp_obj.keys().zip(smtp_obj.values()) {
-						match key.as_str() {
-							"error" => error = Some(val.to_string()),
-							"can_connect_smtp" => {
-								smtp_can_connect = val
-									.as_bool()
-									.ok_or("can_connect_smtp should be a boolean")?
-							}
-							"has_full_inbox" => {
-								smtp_has_full_inbox =
-									val.as_bool().ok_or("has_full_inbox should be a boolean")?
-							}
-							"is_catch_all" => {
-								smtp_is_catch_all =
-									val.as_bool().ok_or("is_catch_all should be a boolean")?
-							}
-							"is_deliverable" => {
-								smtp_is_deliverable =
-									val.as_bool().ok_or("is_deliverable should be a boolean")?
-							}
-							"is_disabled" => {
-								smtp_is_disabled =
-									val.as_bool().ok_or("is_disabled should be a boolean")?
-							}
-							_ => {}
-						}
-					}
-				}
-				"syntax" => {
-					let syntax_obj = val.as_object().ok_or("syntax field should be an object")?;
-					for (key, val) in syntax_obj.keys().zip(syntax_obj.values()) {
-						match key.as_str() {
-							"error" => error = Some(val.to_string()),
-							"is_valid_syntax" => {
-								syntax_is_valid_syntax =
-									val.as_bool().ok_or("is_valid_syntax should be a boolean")?
-							}
-							"username" => {
-								syntax_username = val
-									.as_str()
-									.ok_or("username should be a string")?
-									.to_string()
-							}
-							"domain" => {
-								syntax_domain =
-									val.as_str().ok_or("domain should be a string")?.to_string()
-							}
-							_ => {}
-						}
-					}
-				}
-				// ignore unknown fields
-				_ => {}
-			}
-		}
+
+		let input = top_level
+			.get("input")
+			.and_then(|v| v.as_str())
+			.ok_or("input should be a string")?
+			.to_string();
+		let is_reachable = top_level
+			.get("is_reachable")
+			.and_then(|v| v.as_str())
+			.ok_or("is_reachable should be a string")?
+			.to_string();
+
+		let misc = top_level
+			.get("misc")
+			.and_then(|v| v.as_object())
+			.ok_or("misc field should be an object")?;
+		let misc_is_disposable = misc
+			.get("is_disposable")
+			.and_then(|v| v.as_bool())
+			.ok_or("is_disposable should be a boolean")?;
+		let misc_is_role_account = misc
+			.get("is_role_account")
+			.and_then(|v| v.as_bool())
+			.ok_or("is_role_account should be a boolean")?;
+		let misc_gravatar_url = misc
+			.get("gravatar_url")
+			.and_then(|v| v.as_str())
+			.map(|s| s.to_string());
+
+		let mx = top_level
+			.get("mx")
+			.and_then(|v| v.as_object())
+			.ok_or("mx field should be an object")?;
+		let mx_accepts_mail = mx
+			.get("accepts_email")
+			.and_then(|v| v.as_bool())
+			.ok_or("accepts_email should be a boolean")?;
+
+		let smtp = top_level
+			.get("smtp")
+			.and_then(|v| v.as_object())
+			.ok_or("smtp field should be an object")?;
+		let smtp_can_connect = smtp
+			.get("can_connect_smtp")
+			.and_then(|v| v.as_bool())
+			.ok_or("can_connect_smtp should be a boolean")?;
+		let smtp_has_full_inbox = smtp
+			.get("has_full_inbox")
+			.and_then(|v| v.as_bool())
+			.ok_or("has_full_inbox should be a boolean")?;
+		let smtp_is_catch_all = smtp
+			.get("is_catch_all")
+			.and_then(|v| v.as_bool())
+			.ok_or("is_catch_all should be a boolean")?;
+		let smtp_is_deliverable = smtp
+			.get("is_deliverable")
+			.and_then(|v| v.as_bool())
+			.ok_or("is_deliverable should be a boolean")?;
+		let smtp_is_disabled = smtp
+			.get("is_disabled")
+			.and_then(|v| v.as_bool())
+			.ok_or("is_disabled should be a boolean")?;
+
+		let syntax = top_level
+			.get("syntax")
+			.and_then(|v| v.as_object())
+			.ok_or("syntax field should be an object")?;
+		let syntax_is_valid_syntax = syntax
+			.get("is_valid_syntax")
+			.and_then(|v| v.as_bool())
+			.ok_or("is_valid_syntax should be a boolean")?;
+		let syntax_domain = syntax
+			.get("domain")
+			.and_then(|v| v.as_str())
+			.ok_or("domain should be a string")?
+			.to_string();
+		let syntax_username = syntax
+			.get("username")
+			.and_then(|v| v.as_str())
+			.ok_or("username should be a string")?
+			.to_string();
+
+		let error = top_level.get("error").map(|v| v.to_string());
 
 		Ok(CsvResponse {
 			input,
@@ -196,8 +159,8 @@ impl TryFrom<CsvWrapper> for CsvResponse {
 			smtp_is_catch_all,
 			smtp_is_deliverable,
 			smtp_is_disabled,
-			syntax_domain,
 			syntax_is_valid_syntax,
+			syntax_domain,
 			syntax_username,
 			error,
 		})
