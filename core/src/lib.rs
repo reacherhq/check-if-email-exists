3ea63b5690c05638d364049f66e801c426e401a6
Amaury <1293565+amaury1093@users.noreply.github.com>
2025-03-24 11:04:37 +0100

    fix: Revert back to using lowest-priority MX record

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -232,38 +232,15 @@ pub async fn check_email(input: &CheckEmailInput) -> CheckEmailOutput {
 		"Found misc details"
 	);
 
-	// From the list of MX records, we only choose one: we don't choose the
-	// first or last ones, because some domains put dummy MX records at the
-	// beginning or end of the list (sorted by priority). Instead, we choose a
-	// random one in the middle of the list.
-	//
-	// See here for explanation: https://cwiki.apache.org/confluence/display/SPAMASSASSIN/OtherTricks
-	//
-	// If anyone has a better algorithm, let me know by creating an issue on
-	// Github.
-	// ref: https://github.com/reacherhq/check-if-email-exists/issues/1049
-	let mut mx_records = my_mx
+	// From the list of MX records, we choose the one with the lowest priority.
+	let mx_records = my_mx
 		.lookup
 		.as_ref()
 		.expect("If lookup is error, we already returned. qed.")
 		.iter()
-		// Don't try to connect to honey pot servers.
-		.filter(|a| {
-			!has_rule(
-				&my_syntax.domain,
-				&a.exchange().to_string(),
-				&Rule::HoneyPot,
-			)
-		})
-		.collect::<Vec<&MX>>();
-	mx_records.sort_by_key(|a| a.preference());
-	let host = if mx_records.len() >= 3 {
-		let mut rng = rand::thread_rng();
-		let index = rng.gen_range(1..mx_records.len() - 1);
-		mx_records[index]
-	} else {
-		mx_records[mx_records.len() - 1]
-	};
+		.min_by_key(|a| a.preference())
+		.expect("There should be at least one MX record after filtering.");
+	let host = mx_records;
 
 	let (my_smtp, smtp_debug) = check_smtp(
 		my_syntax
