916ac9dccae20b25c5e1a8fa5c19b512072c9a50
Krishna Kumar <krishnakumar@infynd.com>
2026-01-15 14:08:33 +0530

    Fix lints

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -35,8 +35,8 @@ async fn main() -> Result<(), anyhow::Error> {
 	info!(target: LOG_TARGET, version=?CARGO_PKG_VERSION, "Running Reacher");
 	let mut config = load_config().await?;
 	config.connect().await?;
-	
-    // SECURITY: Don't log full config as it likely contains secrets (DB_URL, etc)
+
+	// SECURITY: Don't log full config as it likely contains secrets (DB_URL, etc)
 	debug!(target: LOG_TARGET, "{:#?}", config.get_verif_method());
 
 	// Setup sentry bug tracking.
@@ -60,4 +60,4 @@ async fn main() -> Result<(), anyhow::Error> {
 	info!("Shutting down...");
 
 	Ok(())
-} 
+}
